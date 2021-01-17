1377da5d95bcedda151789ab5df167638fcb3648
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-01-17 19:13:32 -0300

    adicionar

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python3 manage.py migrate
+web: gunicorn site_ester.wsgi --preload --log-file –
---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Main
+
+def Index(request):
+    busca = request.GET.get('search')
+    page = request.GET.get('page')
+    if busca:
+        main = Main.objects.filter(nome__icontains=busca)
+        paginator = Paginator(main, 3)
+        main = paginator.get_page(page)
+        
+        
+    else:
+        main_list = Main.objects.all()
+        paginator = Paginator(main_list, 3)
+
+
+        main = paginator.get_page(page)
+    
+    return render(request, 'index.html',
+    {'main':main}) 
